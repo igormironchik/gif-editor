f48fe246f27f43e67b344e985469e752b327be9d
Igor Mironchik <igor.mironchik@gmail.com>
2023-09-28 09:24:47 +0300

    Add license of giflib.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1087,6 +1087,24 @@ MainWindow::licenses()
 		"    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the\n"
 		"    License for the specific language governing permissions and limitations\n"
 		"    under the License.</p>" ) );
+	msg.addLicense( QStringLiteral( "giflib" ),
+		QStringLiteral( "<p><b>giflib License</b></p>\n\n"
+		"<p>The GIFLIB distribution is Copyright (c) 1997  Eric S. Raymond</p>\n\n"
+		"<p>Permission is hereby granted, free of charge, to any person obtaining a copy "
+		"of this software and associated documentation files (the \"Software\"), to deal "
+		"in the Software without restriction, including without limitation the rights "
+		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell "
+		"copies of the Software, and to permit persons to whom the Software is "
+		"furnished to do so, subject to the following conditions:</p>\n\n"
+		"<p>The above copyright notice and this permission notice shall be included in "
+		"all copies or substantial portions of the Software.</p>\n\n"
+		"<p>THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
+		"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
+		"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE "
+		"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+		"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
+		"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN "
+		"THE SOFTWARE.</p>" ) );
 
 	msg.exec();
 }
